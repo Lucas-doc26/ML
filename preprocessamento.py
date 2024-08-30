376c0e29c7eef7b80f5eab2980dec2215f9df605
Lucas Cunha <lucaas.doc26@gmail.com>
2024-08-30 15:37:09 -0300

    Initial commit

---
+++ preprocessamento.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from typing import List, Tuple
+import os
+
+def preprocessamento(caminho: str, proporcao_treino: float = 0.6, proporcao_teste: float = 0.2, proporcao_validacao: float = 0.2, autoencoder: bool = False):
+    """
+    Ao passar um dataFrame .csv, ele irá retornar geradores de dados para treino, teste e validação + os 3 .csv dividos igualmente os geradores.
+    
+    Parâmetros:
+        caminho (str): Caminho para o arquivo CSV.
+        proporcao_treino (float): Proporção de dados de treino.
+        proporcao_teste (float): Proporção de dados de teste.
+        proporcao_validacao (float): Proporção de dados de validação.
+        autoencoder (bool): Se True, prepara os dados para um autoencoder (class_mode='input').
+                            Se False, prepara os dados para classificação binária (class_mode='binary').
+    
+    Retorna:
+        treino_gerador, validacao_gerador, teste_gerador, treino, teste, validacao
+    """
+    dataframe = pd.read_csv(caminho)
+
+    if not autoencoder:
+        dataframe['classe'] = dataframe['classe'].astype(str)
+
+    treino, teste = train_test_split(dataframe, test_size=proporcao_teste, random_state=42)
+    treino, validacao = train_test_split(treino, test_size=proporcao_validacao / (1 - proporcao_teste), random_state=42)
+
+    img_width, img_height = 256, 256
+    batch_size = 32
+
+    preprocess_input = tf.keras.applications.mobilenet_v3.preprocess_input
+    treino_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+    validacao_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+    teste_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+    class_mode = 'input' if autoencoder else 'binary'
+
+    treino_gerador = treino_datagen.flow_from_dataframe(
+        dataframe=treino,
+        x_col='caminho_imagem',
+        y_col='caminho_imagem' if autoencoder else 'classe', #Usar a imagem como saída se for autoencoder
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=class_mode,  #Class mode baseado no parâmetro autoencoder
+        shuffle=True
+    )
+
+    validacao_gerador = validacao_datagen.flow_from_dataframe(
+        dataframe=validacao,
+        x_col='caminho_imagem',
+        y_col='caminho_imagem' if autoencoder else 'classe',  
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=class_mode, 
+        shuffle=True
+    )
+
+    teste_gerador = teste_datagen.flow_from_dataframe(
+        dataframe=teste,
+        x_col='caminho_imagem',
+        y_col='caminho_imagem' if autoencoder else 'classe',  
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=class_mode, 
+        shuffle=False
+    )
+
+    return treino_gerador, validacao_gerador, teste_gerador, treino, teste, validacao
+
+def preprocessamento_dataframe(caminho:str):
+    """
+    Retorna o dataFrame gerado pelo ImageDataGenerator e o dataFrame com os caminhos e classe
+    """
+    
+    img_width, img_height = 256, 256
+    batch_size = 32
+    dataframe = pd.read_csv(caminho)
+    preprocess_input = tf.keras.applications.mobilenet_v3.preprocess_input
+    datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+    dataframe_gerador = datagen.flow_from_dataframe(
+            dataframe=dataframe,
+            x_col='caminho_imagem',
+            y_col='classe',
+            target_size=(img_width, img_height),
+            batch_size=batch_size,
+            class_mode='binary'
+        )
+    
+    return dataframe, dataframe_gerador
+
+def carregar_e_preprocessar_imagens(caminhos_imagens, target_size=(256, 256)):
+    """
+    Carrega e processa imagens a partir de caminhos fornecidos. Retornando um array numpy contendo todas as imgs. 
+
+    Como usar:
+    - caminhos_imagens = dataset_df['caminho_imagem'].tolist() 
+    - passa a variável como argumento 
+    """
+
+    imagens = []
+    for caminho in caminhos_imagens:
+        img = load_img(caminho, target_size=target_size)
+        img_array = img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        imagens.append(img_array)
+    return np.vstack(imagens)
+
+# Exemplo de uso:
+# caminhos_imagens = dataset_df['caminho_imagem'].tolist() 
+# imagens = carregar_e_preprocessar_imagens(caminhos_imagens)
+# modelo.predict(imagens).argmax(axis=1) -> assim que faz a previsão 
+
+def mapear_rotulos_binarios(classes):
+    """
+    Converte as classes em binários: 
+    - Occupied vira 1 
+    - Empty vira 0
+    """
+    return np.array([1 if classe == 'Occupied' else 0 for classe in classes])
+
+# Exemplo de uso:
+
+def preprocessamento_dataframe_unico(caminho_csv: str, autoencoder: bool = False):
+    """
+    Processa um arquivo CSV para criar um fluxo de imagens para treinamento usando ImageDataGenerator.
+
+    :param caminho_csv: Caminho para o arquivo CSV contendo informações das imagens.
+    :param autoencoder: Se True, configura o modo autoencoder; caso contrário, configuração de classificação binária.
+    :return: Um objeto DataFrameIterator do ImageDataGenerator.
+    """
+    dataframe = pd.read_csv(caminho_csv)
+
+    img_width, img_height = 256, 256
+    batch_size = 32
+
+    preprocess_input = tf.keras.applications.mobilenet_v3.preprocess_input
+
+    datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+    # Define o class_mode
+    class_mode = 'input' if autoencoder else 'binary'
+
+    Dataframe_preprocessado = datagen.flow_from_dataframe(
+        dataframe=dataframe,
+        x_col='caminho_imagem',
+        y_col='caminho_imagem' if autoencoder else 'classe',
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=class_mode,
+        shuffle=True
+    )
+
+    return Dataframe_preprocessado, dataframe
+
+def todos_df(autoencoder: bool = False):
+    pasta = 'Datasets_csv'
+    for csv in os.listdir(pasta):
+        caminho_csv = os.path.join(pasta, csv)
+        if os.path.isfile(caminho_csv) and csv.endswith('.csv'):
+            print(f'Processando o arquivo: {csv}')
+            return preprocessamento_dataframe_unico(caminho_csv, autoencoder)
+
---
+++ segmentandoDatasets.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import os
+import random
+import csv
+from typing import Tuple, Optional
+
+def segmentando_datasets(quantidade_PUC:int=None, quantidade_UFPR04:int=None, quantidade_UFPR05:int=None) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Função para criar os datasets csv.\n
+    Caso queira eles como lista, retorna uma tupla com os datasets separados em ordem PUC, UFPR04, UFPR05
+    """
+    faculdades = ['PUC', 'UFPR04', 'UFPR05']
+    
+    limites_padrao = {
+        'PUC': float('inf'),  # Infinito para capturar todas as imagens
+        'UFPR04': float('inf'),
+        'UFPR05': float('inf')
+    }
+    
+    if quantidade_PUC is not None:
+        limites_padrao['PUC'] = quantidade_PUC
+    if quantidade_UFPR04 is not None:
+        limites_padrao['UFPR04'] = quantidade_UFPR04
+    if quantidade_UFPR05 is not None:
+        limites_padrao['UFPR05'] = quantidade_UFPR05
+
+    tempos = ['Cloudy', 'Rainy', 'Sunny']
+    
+    dataframes = [] 
+
+    for local in faculdades:
+        caminhos_imagem = []
+        classes = []
+        for tempo in tempos:
+            sample_dir = os.path.join(
+                r"/home/lucas/Downloads/PKLot/PKLotSegmented/",
+                local, tempo)
+                #"C:\Users\lucaa\Downloads\PKLot\PKLot\PKLotSegmented"
+                #/home/lucas/Downloads/PKLot/PKLotSegmented/
+            pastas = os.listdir(sample_dir)
+            
+            if not os.path.exists(sample_dir)   :
+                print(f'Diretório não encontrado: {sample_dir}')
+
+            for pasta in pastas:
+                todos_arquivos = []
+                for class_dir in ['Empty', 'Occupied']:
+                    # Caminho completo para o subdiretório da classe
+                    full_class_dir = os.path.join(sample_dir, pasta, class_dir)
+                    if os.path.exists(full_class_dir):
+                        for file in os.listdir(full_class_dir):
+                            if file.endswith('.jpg'):
+                                todos_arquivos.append((os.path.join(full_class_dir, file), class_dir))
+
+                random.shuffle(todos_arquivos)
+
+                # Adicionando todas as imagens ao DataFrame
+                for file_path, class_dir in todos_arquivos:
+                    caminhos_imagem.append(file_path)
+                    classes.append(class_dir)
+
+        # Embaralhando novamente para garantir a aleatoriedade
+        combined_data = list(zip(caminhos_imagem, classes))
+        random.shuffle(combined_data)
+        caminhos_imagem, classes = zip(*combined_data)
+
+        limite_arquivos = min(limites_padrao[local], len(caminhos_imagem))
+
+        caminhos_imagem = caminhos_imagem[:limite_arquivos]
+        classes = classes[:limite_arquivos]
+
+        df = pd.DataFrame({
+            'caminho_imagem': caminhos_imagem,
+            'classe': classes
+        })
+
+        # Salvar o DataFrame como arquivo CSV
+        csv_path = f'Datasets_csv/df_{local}.csv'
+        df.to_csv(csv_path, index=False)
+        print(f'DataFrame do local {local} salvo como: {csv_path}')
+
+        # Adicionando o DataFrame à lista
+        dataframes.append(df)
+
+        print(f'DataFrame do local {local}:')
+        print(df.head())
+        print('\n')
+
+# Exemplo de usos: 
+#dataframePuc1, dataframeUFPR041, dataframeUFPR051 = segmentadando_datasets() -> Cria o csv/var com todos os arquivos 
+#dataframePuc, dataframeUFPR04, dataframeUFPR05 = segmentadando_datasets(1000, 1000, 1000) -> Cria o csv/var com o limite
+segmentando_datasets() #-> Cria somente os csv 
