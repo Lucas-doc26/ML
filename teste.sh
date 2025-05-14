1688b5827cfd1dbb2af736845853663e82442620
lucas <lucaas.ocunha@gmail.com>
2025-05-14 17:35:19 -0300

    README att

--- teste.sh
+++
@@ -1,6 +0,0 @@
-#Treinando o autoencoder que faltava
-python3 treino_autoencoder.py Modelo_Kyoto 10 PKLOT '(64,64)' 50
-
-python3 treina_classificador.py Modelo_Kyoto 10 PKLot '(64,64)' PUC 20
-python3 treina_classificador.py Modelo_Kyoto 10 PKLot '(64,64)' UFPR04 20
-python3 treina_classificador.py Modelo_Kyoto 10 PKLot '(64,64)' UFPR05 20
